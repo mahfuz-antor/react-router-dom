15231ebd2aa58adf1423a3cdae7dffa53d9d33fa
mahfuz-antor <antorcnj2014@gmail.com>
2023-01-17 00:32:18 +0600

    tailwind css added

---
+++ src/components/AdminPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <>
+      <h2>Admin Page here.</h2>
+    </>
+  );
+};
+
+export default AdminPage;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h2 className="text-center text-3xl text-sky-500">Home page this is</h2>
+    </>
+  );
+};
+
+export default Home;
